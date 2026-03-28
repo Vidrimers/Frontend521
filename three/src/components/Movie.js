f1381b00c303f4c4b3790da9f98eb69d070e9c96
Vidrimers <vidrimers2@gmail.com>
2026-03-29 00:37:49 +0300

    Правка img card

--- three/src/components/Movie.js
+++ three/src/components/Movie.js
@@ -1,14 +1,21 @@
 import "./Movie.css";
 
 function Movie(props) {
-  const { Title, Year, imdbID, Type, Poster } = props;
+  const { Title, Year, Type, Poster } = props;
   return (
     <div className="card">
-      {Poster === "N/A" ? (
-        <img src={"https://picsum.photos/300/400/?blur"} />
+      {Poster === "N/A" ||
+      Poster ===
+        "https://m.media-amazon.com/images/M/MV5BZGY1NmNlZDUtNDRkYy00YTE0LWJhZmQtMDU3ZGMwYzliYjAwXkEyXkFqcGdeQXVyMjU0MzI5MQ@@._V1_SX300.jpg" ||
+      Poster ===
+        "https://m.media-amazon.com/images/M/MV5BMThlNDc4MGUtMDM1NS00MTFlLTgzMzYtMGIwZDNhMWM3MGM5XkEyXkFqcGdeQXVyNz9SMTExNTQ@._V1_SX300.jpg" ||
+      Poster ===
+        "https://m.media-amazon.com/images/M/MV5BNWM3MDU2MWQtYjdlNC00NDBlLTkyNGMtNjdhYjd1NTdiNTFlXkEyXkFqcGdeQXVyNTEwNDY2MjU@._V1_SX300.jpg" ? (
+        <img src={"https://picsum.photos/300/400?blur"} alt="Poster" />
       ) : (
-        <img src={Poster} alt="" />
+        <img src={Poster} alt="Poster" />
       )}
+
       <div>
         <h3>{Title}</h3>
         <p>
